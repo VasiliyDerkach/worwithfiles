f6adc4776ffa97b85a6e61adbc89182736756cc1
Vasiliy <v_derkach@inbox.ru>
2024-07-03 21:32:14 +0500

    56

--- WordsFinderunit.py
+++ WordsFinderunit.py
@@ -8,21 +8,36 @@ class WordsFinder:
         all_words={}
         for file in self.file_names:
             file_lst_words = []
-            with open(file,'r','utf-8') as vfile:
-                txtfile = vfile.read()
-                for line in txtfile:
+            with open(file, mode='r',encoding='utf-8') as vfile:
+
+                for line in vfile:
                     line = line.lower()
+                    #print(line,end='')
                     for char in punktus:
-                        line.replace(char, '')
+                        line = line.replace(char, '')
+                    #print(line, end='')
                     lst_words = line.split()
+                    #print(lst_words)
                     file_lst_words+= lst_words
                 vfile.close()
             all_words[file]=file_lst_words
+        return all_words
+    def find(self, word):
+        dct = {}
+        for key_file, words_of_file in self.get_all_words():
+            i_word= words_of_file.index(word.lower())
+            if i_word>=0:
+                dct[key_file]=i_word
+        return dct
+
 
 
 if __name__== '__main__':
-    tst = WordsFinder('fhggjf','858595690')
+    tst = WordsFinder('abra.txt','cadabra.txt')
+    dct = tst.get_all_words()
+    g= ['gg','oo1t','tyu','oot']
+    print(g.index('oot'))
+    #print(dct)
     # for u in tst.file_names:
     #     print(u)
-    # p = ' fgkgjdfl  fkggjg  rgeptotoert ;rgker;gg'
-    # print(p.split())
+    #
